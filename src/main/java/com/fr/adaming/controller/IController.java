47bb323ed6b3b59438233ffca371ee2fd4522579
unknown <gregoire_brebner@hotmail.com>
2020-03-13 17:17:48 +0100

    IController et ResponseDto

--- src/main/java/com/fr/adaming/controller/IController.java
+++ src/main/java/com/fr/adaming/controller/IController.java
@@ -3,29 +3,30 @@ package com.fr.adaming.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fr.adaming.dto.ResponseDto;
+
 @RequestMapping
 public interface IController<CT, T> { // CT: CreateDto. T: DTO complet avec id
 	
 	@PostMapping
-	public ResponseEntity<?> create(@RequestBody CT dto);
+	public ResponseEntity<ResponseDto> create(@RequestBody CT dto);
 
 	@DeleteMapping
-	public ResponseEntity<?> deleteById(@RequestParam(name = "id") int id) ;
+	public ResponseEntity<ResponseDto> deleteById(@RequestParam(name = "id") int id) ;
 
 	@PutMapping
-	public ResponseEntity<?> update(@RequestBody T dto) ;
+	public ResponseEntity<ResponseDto> update(@RequestBody T dto) ;
 
 	@GetMapping(path = "/one")
-	public ResponseEntity<?> readById(@RequestParam(name = "id") int id) ;
+	public ResponseEntity<ResponseDto> readById(@RequestParam(name = "id") int id) ;
 
 	@GetMapping(path = "/all")
-	public ResponseEntity<?> readAll();
+	public ResponseEntity<ResponseDto> readAll();
 
 }
