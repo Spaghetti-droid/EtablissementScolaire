c7fb58e0e26eef24c1695ac67717244bb342ad8c
Spaghetti-droid <62012642+Spaghetti-droid@users.noreply.github.com>
2020-03-13 14:24:44 +0100

    Ajout d'un projet vide

---
+++ src/main/java/com/fr/adaming/EtablissementScolaireApplication.java
@@ -0,0 +1,13 @@
+package com.fr.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtablissementScolaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtablissementScolaireApplication.class, args);
+	}
+
+}
