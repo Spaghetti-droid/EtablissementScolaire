b5e67ff137a9c94a80711b14167fc69cfa57ccec
Lea369 <coston.lea@gmail.com>
2020-03-16 13:17:30 +0100

    changement Notes

--- src/main/java/com/fr/adaming/converter/NoteConverter.java
+++ src/main/java/com/fr/adaming/converter/NoteConverter.java
@@ -3,75 +3,134 @@ package com.fr.adaming.converter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.ServiceNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fr.adaming.dto.ClasseUpdateDto;
+import com.fr.adaming.dto.NoteUpdateDto;
+import com.fr.adaming.dto.NoteCreateDto;
 import com.fr.adaming.dto.NoteCreateDto;
 import com.fr.adaming.dto.NoteUpdateDto;
 import com.fr.adaming.entity.Classe;
+import com.fr.adaming.entity.Etudiant;
+import com.fr.adaming.entity.Matiere;
 import com.fr.adaming.entity.Note;
+import com.fr.adaming.entity.Note;
+import com.fr.adaming.entity.Note;
+import com.fr.adaming.service.IEtudiantService;
+import com.fr.adaming.service.IExamenService;
 
-public class NoteConverter {
 
-	public static Note convertNoteCreateDtoToNote(NoteCreateDto dto) {
+public class NoteConverter implements IConverter<NoteCreateDto, NoteUpdateDto, Note>{
+	
+	@Autowired
+	private IEtudiantService serviceE;
+	
+	@Autowired
+	private IExamenService serviceExam;
 
-		if (dto == null) {
+	@Override
+	public Note convertCreateDtoToEntity(NoteCreateDto createDto) {
+		if (createDto == null) {
 			return null;
 		}
 		Note n = new Note();
-		n.setValeur(dto.getValue());
-
+		n.setValeur(createDto.getValue());
+		n.setEtudiant(serviceE.readByEmail(createDto.getNometudiant()));
+		n.setExamen(serviceExam.readById(createDto.getIdexamen()));
+		
 		return n;
 	}
 
-	public static NoteCreateDto convertNoteToNoteCreateDto(Note note) {
-		if (note == null) {
+	@Override
+	public NoteCreateDto convertEntityToCreateDto(Note entity) {
+		if(entity == null) {
 			return null;
-
 		}
-		NoteCreateDto dto = new NoteCreateDto();
-		dto.setValue(note.getValeur());
-		return dto;
+		NoteCreateDto n = new NoteCreateDto();
+		n.setIdexamen(entity.getExamen().getId());
+		n.setNometudiant(entity.getEtudiant().getEmail());
+		n.setValue(entity.getValeur());
+		
+		return n;
 	}
 
-	public static Note convertNoteUpdateDtoToNote(NoteUpdateDto dto) {
-
-		if (dto == null) {
+	@Override
+	public Note convertUpdateDtoToEntity(NoteUpdateDto updateDto) {
+		if (updateDto == null) {
 			return null;
 		}
 		Note n = new Note();
-		n.setValeur(dto.getValue());
-
+		n.setValeur(updateDto.getValue());
+		n.setEtudiant(serviceE.readByEmail(updateDto.getNometudiant()));
+		n.setExamen(serviceExam.readById(updateDto.getIdexamen()));
+		n.setId(updateDto.getId());
+		
 		return n;
 	}
 
-	public static NoteUpdateDto convertNoteToNoteUpdateDto(Note note) {
-		if (note == null) {
-			return null;
-
-		}
-		NoteUpdateDto dto = new NoteUpdateDto();
-		dto.setValue(note.getValeur());
-		return dto;
-	}
-
-	public static List<Note> convertListNoteUpdateDtoToListNote(List<NoteUpdateDto> listDto) {
-		if (listDto == null) {
+	@Override
+	public NoteUpdateDto convertEntityToUpdateDto(Note entity) {
+		if(entity == null) {
 			return null;
 		}
-		List<Note> list = new ArrayList<Note>();
-		for (NoteUpdateDto dto : listDto) {
-			list.add(convertNoteUpdateDtoToNote(dto));
-		}
-		return list;
+		NoteUpdateDto n = new NoteUpdateDto();
+		n.setIdexamen(entity.getExamen().getId());
+		n.setNometudiant(entity.getEtudiant().getEmail());
+		n.setValue(entity.getValeur());
+		n.setId(entity.getId());
+		
+		return n;
 	}
 
-	public static List<NoteUpdateDto> convertListNoteToListNotUpdateDto(List<Note> list) {
-		if (list == null) {
+	@Override
+	public List<Note> convertListCreateDtoToEntity(List<NoteCreateDto> listeCreateDto) {
+		if (listeCreateDto == null) {
 			return null;
 		}
-		List<NoteUpdateDto> listDto = new ArrayList<NoteUpdateDto>();
-		for (Note note : list) {
-			listDto.add(convertNoteToNoteUpdateDto(note));
+		List<Note> n = new ArrayList<Note>();
+		for(NoteCreateDto e : listeCreateDto) {
+			n.add(convertCreateDtoToEntity(e));
 		}
-		return listDto;
+		return n;
 	}
+
+	@Override
+	public List<NoteCreateDto> convertListEntityToCreateDto(List<Note> listeEntity) {
+		if (listeEntity == null) {
+			return null;
+		}
+		List<NoteCreateDto> notes = new ArrayList<NoteCreateDto>();
+		for(Note e : listeEntity) {
+			notes.add(convertEntityToCreateDto(e));
+		}
+		return notes;
+	}
+
+	@Override
+	public List<Note> convertListUpdateDtoToEntity(List<NoteUpdateDto> listeUpdateDto) {
+		if (listeUpdateDto == null) {
+			return null;
+		}
+		List<Note> n = new ArrayList<Note>();
+		for(NoteUpdateDto e : listeUpdateDto) {
+			n.add(convertUpdateDtoToEntity(e));
+		}
+		return n;
+	}
+
+	@Override
+	public List<NoteUpdateDto> convertListEntityToUpdateDto(List<Note> listeEntity) {
+		if (listeEntity == null) {
+			return null;
+		}
+		List<NoteUpdateDto> notes = new ArrayList<NoteUpdateDto>();
+		for(Note e : listeEntity) {
+			notes.add(convertEntityToUpdateDto(e));
+		}
+		return notes;
+	}
+
+	
 }
