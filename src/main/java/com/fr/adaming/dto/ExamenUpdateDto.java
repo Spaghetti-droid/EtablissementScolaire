dbf38fc9b8723a83bfb472a8aa62bc632e3d9460
Lea369 <coston.lea@gmail.com>
2020-03-16 09:59:39 +0100

    examen sans rreur

--- src/main/java/com/fr/adaming/dto/ExamenUpdateDto.java
+++ src/main/java/com/fr/adaming/dto/ExamenUpdateDto.java
@@ -1,5 +1,17 @@
 package com.fr.adaming.dto;
 
+import java.time.LocalDate;
+
+import com.fr.adaming.enumeration.Type;
+
 public class ExamenUpdateDto {
 
+private LocalDate dateExamen;
+	
+	private Type typeExamen;
+	
+	private double coefExamen;
+	
+	private String matiere;
+	
 }
