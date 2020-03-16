8c08d3c42dcf335e118f36f2527865d934376961
Lea369 <coston.lea@gmail.com>
2020-03-16 10:22:16 +0100

    Examen converter et dto

--- src/main/java/com/fr/adaming/converter/ExamenConverter.java
+++ src/main/java/com/fr/adaming/converter/ExamenConverter.java
@@ -1,20 +1,30 @@
 package com.fr.adaming.converter;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fr.adaming.dto.ClasseCreateDto;
 import com.fr.adaming.dto.ExamenCreateDto;
 import com.fr.adaming.dto.ExamenUpdateDto;
 import com.fr.adaming.entity.Examen;
+import com.fr.adaming.entity.Matiere;
+import com.fr.adaming.service.IMatiereService;
+import com.fr.adaming.service.MatiereService;
 
 public class ExamenConverter implements IConverter<ExamenCreateDto, ExamenUpdateDto, Examen> {
 
+	@Autowired
+	MatiereService serviceMat;
+	
 	@Override
 	public Examen convertCreateDtoToEntity(ExamenCreateDto createDto) {
 		Examen exam = new Examen();
 		exam.setCoef(createDto.getCoefExamen());
 		exam.setDate(createDto.getDateExamen());
-
+		Matiere mat =serviceMat.readByNom(createDto.getMatiereExamen());
+		exam.setMatiere(mat);
 		exam.setType(createDto.getTypeExamen());
 		return exam;
 	}
@@ -22,44 +32,72 @@ public class ExamenConverter implements IConverter<ExamenCreateDto, ExamenUpdate
 	@Override
 	public ExamenCreateDto convertEntityToCreateDto(Examen entity) {
 		ExamenCreateDto examdto = new ExamenCreateDto();
+		examdto.setCoefExamen(entity.getCoef());
+		examdto.setDateExamen(entity.getDate());
+		examdto.setMatiereExamen(entity.getMatiere().getNom());
+		examdto.setTypeExamen(entity.getType());
 		
-		return null;
+		return examdto;
 	}
 
 	@Override
 	public Examen convertUpdateDtoToEntity(ExamenUpdateDto updateDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Examen exam = new Examen();
+		exam.setCoef(updateDto.getCoefExamen());
+		exam.setDate(updateDto.getDateExamen());
+		Matiere mat =serviceMat.readByNom(updateDto.getMatiereExamen());
+		exam.setMatiere(mat);
+		exam.setType(updateDto.getTypeExamen());
+		exam.setId(updateDto.getIdExam());
+		return exam;
 	}
 
 	@Override
 	public ExamenUpdateDto convertEntityToUpdateDto(Examen entity) {
-		// TODO Auto-generated method stub
-		return null;
+		ExamenUpdateDto examdto = new ExamenUpdateDto();
+		examdto.setCoefExamen(entity.getCoef());
+		examdto.setDateExamen(entity.getDate());
+		examdto.setMatiereExamen(entity.getMatiere().getNom());
+		examdto.setTypeExamen(entity.getType());
+		examdto.setIdExam(entity.getId());
+		
+		return examdto;
 	}
 
 	@Override
 	public List<Examen> convertListCreateDtoToEntity(List<ExamenCreateDto> listeCreateDto) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Examen> exams = new ArrayList<Examen>();
+		for(ExamenCreateDto e : listeCreateDto) {
+			exams.add(convertCreateDtoToEntity(e));
+		}
+		return exams;
 	}
 
 	@Override
 	public List<ExamenCreateDto> convertListEntityToCreateDto(List<Examen> listeEntity) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ExamenCreateDto> exams = new ArrayList<ExamenCreateDto>();
+		for(Examen e : listeEntity) {
+			exams.add(convertEntityToCreateDto(e));
+		}
+		return exams;
 	}
 
 	@Override
 	public List<Examen> convertListUpdateDtoToEntity(List<ExamenUpdateDto> listeUpdateDto) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Examen> exams = new ArrayList<Examen>();
+		for(ExamenUpdateDto e : listeUpdateDto) {
+			exams.add(convertUpdateDtoToEntity(e));
+		}
+		return exams;
 	}
 
 	@Override
 	public List<ExamenUpdateDto> convertListEntityToUpdateDto(List<Examen> listeEntity) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ExamenUpdateDto> exams = new ArrayList<ExamenUpdateDto>();
+		for(Examen e : listeEntity) {
+			exams.add(convertEntityToUpdateDto(e));
+		}
+		return exams;
 	}
 
 	
--- src/main/java/com/fr/adaming/dto/ExamenCreateDto.java
+++ src/main/java/com/fr/adaming/dto/ExamenCreateDto.java
@@ -1,6 +1,9 @@
 package com.fr.adaming.dto;
 
 import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+
 import com.fr.adaming.entity.Matiere;
 import com.fr.adaming.enumeration.Type;
 
@@ -10,12 +13,14 @@ import lombok.Data;
 @Data
 public class ExamenCreateDto {
 	
+	@NotBlank
 	private LocalDate dateExamen;
 	
 	private Type typeExamen;
 	
 	private double coefExamen;
 	
+	@NotBlank
 	private String matiereExamen;
 
 }
