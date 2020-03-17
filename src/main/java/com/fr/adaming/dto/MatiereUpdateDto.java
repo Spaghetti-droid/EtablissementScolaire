803e8c4839134d48bc61aa3af251840fcb8c94b7
jmmathevet <jmmathevet@gmail.com>
2020-03-17 13:35:13 +0100

    Modification de MatiereDto et MatiereConverter

--- src/main/java/com/fr/adaming/dto/MatiereUpdateDto.java
+++ src/main/java/com/fr/adaming/dto/MatiereUpdateDto.java
@@ -1,24 +1,25 @@
 package com.fr.adaming.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
-import com.fr.adaming.entity.Etudiant;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class MatiereUpdateDto {
 	
+	@Positive
 	private int idMatiere;
 	
-
+	@NotBlank
 	private String nomMatiere; 
 	
+	private List<EtudiantUpdateDto> listeEtudiant;
 	
-	private List<String> emailListMatiere=new ArrayList<String>();
+	
 
 }
