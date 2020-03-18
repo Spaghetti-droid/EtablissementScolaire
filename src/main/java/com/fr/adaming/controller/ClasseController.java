68908d329b1ec230df6039030ecfbc5be7c6f6b1
jmmathevet <jmmathevet@gmail.com>
2020-03-18 17:49:44 +0100

    Ajout de l'annotation Crossorigin

--- src/main/java/com/fr/adaming/controller/ClasseController.java
+++ src/main/java/com/fr/adaming/controller/ClasseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fr.adaming.converter.IConverter;
@@ -15,6 +16,7 @@ import com.fr.adaming.entity.Classe;
 import com.fr.adaming.service.IClasseService;
 
 @RestController
+@CrossOrigin
 public class ClasseController implements IClasseController {
 
 	@Autowired
--- src/main/java/com/fr/adaming/controller/ExamenController.java
+++ src/main/java/com/fr/adaming/controller/ExamenController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import com.fr.adaming.dto.ResponseDto;
 import com.fr.adaming.service.IExamenService;
 
 @RestController
+@CrossOrigin
 public class ExamenController implements IExamenController {
 	
 	@Autowired
