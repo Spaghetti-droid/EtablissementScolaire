63ef39acf2147336213bc5d39d5a37af7bac6601
jmmathevet <jmmathevet@gmail.com>
2020-03-18 18:16:53 +0100

    Annotation CrossOrigin

--- src/main/java/com/fr/adaming/controller/MatiereController.java
+++ src/main/java/com/fr/adaming/controller/MatiereController.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ import com.fr.adaming.dto.ResponseDto;
 import com.fr.adaming.service.IMatiereService;
 
 @RestController
+@CrossOrigin
 public class MatiereController implements IMatiereController {
 	
 	@Autowired
