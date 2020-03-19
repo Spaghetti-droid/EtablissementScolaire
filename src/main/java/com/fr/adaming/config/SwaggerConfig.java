1d62116887b4f6f2092b24739a4f00d2bcc9588f
Lea369 <coston.lea@gmail.com>
2020-03-19 09:52:29 +0100

    rename swagger config

--- src/main/java/com/fr/adaming/config/SwaggerConfig.java
+++ src/main/java/com/fr/adaming/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.fr.adaming;
+package com.fr.adaming.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {                                    
+public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
