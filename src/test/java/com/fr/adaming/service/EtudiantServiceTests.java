d2ba9d06473a7570e4cce6d67c51fa3c37bc6f33
unknown <gregoire_brebner@hotmail.com>
2020-03-20 15:16:27 +0100

    Etudiant service tests faits

--- src/test/java/com/fr/adaming/service/EtudiantServiceTests.java
+++ src/test/java/com/fr/adaming/service/EtudiantServiceTests.java
@@ -1,10 +1,12 @@
 package com.fr.adaming.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +18,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
+import com.fr.adaming.entity.Absence;
 import com.fr.adaming.entity.Etudiant;
+import com.fr.adaming.entity.Examen;
+import com.fr.adaming.entity.Note;
 
 @SpringBootTest
 public class EtudiantServiceTests {
@@ -126,7 +131,34 @@ public class EtudiantServiceTests {
 
 		assertNull(etuService.readById(3));
 	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(5, 19000205, 69500, 'rodgers@marvel.fr','Rodgers' , 0235645611, 'Steve')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testreadByValidEmail_shouldReturnEtudiant() { 
 
+		Etudiant expectedEtu = new Etudiant(5, 69500, 19000205, 235645611, "rodgers@marvel.fr");
+		expectedEtu.setNom("Rodgers");
+		expectedEtu.setPrenom("Steve");
+		assertEquals(expectedEtu, etuService.readByEmail("rodgers@marvel.fr"));
+		
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(5, 19000205, 69500, 'rodgers@marvel.fr','Rodgers' , 0235645611, 'Steve')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testreadByInvalidEmail_shouldReturnNull() { 
+
+		assertNull(etuService.readByEmail("Aaaaaaaah@marvel.fr"));
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(5, 19000205, 69500, 'rodgers@marvel.fr','Rodgers' , 0235645611, 'Steve')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testreadByNullEmail_shouldReturnNull() { 
+
+		assertNull(etuService.readByEmail(null));
+	}	
 
 	@Test
 	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(5, 19000205, 69500, 'rodgers@marvel.fr','Rodgers' , 0235645611, 'Steve')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
@@ -149,6 +181,30 @@ public class EtudiantServiceTests {
 	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	public void testupdateNullCni_shouldReturnFalse() {
 		Etudiant etu = new Etudiant(8, "Stark", "Tony", 0, "ironman@marvel.fr");
+		assertFalse(etuService.update(etu));
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 0235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testupdateNullEntity_shouldReturnFalse() {
+		assertFalse(etuService.update(null));
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 0235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testupdateInvalidId_shouldReturnFalse() {
+		Etudiant etu = new Etudiant(10001, "Stark", "Tony", 6, "ironman@marvel.fr");
+		assertFalse(etuService.update(etu));
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 0235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(9, 19000205, 69500, 'rodgers@marvel.fr','Rodgers' , 0235645611, 'Steve')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testupdateConflictingCni_shouldReturnFalse() {
+		Etudiant etu = new Etudiant(8, "Stark", "Tony", 19000205, "ironman@marvel.fr");
 		assertFalse(etuService.update(etu));
 	}
 
@@ -195,6 +251,168 @@ public class EtudiantServiceTests {
 		assertFalse(etuService.deleteById(0));
 	}
 
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (1, '2020-05-21', '2020-05-21', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (2, '2020-05-25', '2020-05-25', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from absence", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadAbsenceByValidEmail_shouldReturnList() {
+		
+		Etudiant etu = new Etudiant();
+		etu.setId(8);
+		etu.setCni(545684842);
+		etu.setCp(69500);
+		etu.setEmail("ironman@marvel.fr");
+		etu.setNom("Stark");
+		etu.setPrenom("Tony");
+		etu.setNum(235645611);
+		
+		Absence	a1 = new Absence();
+		a1.setId(1);
+		a1.setDateDebut(LocalDate.parse("2020-05-21"));
+		a1.setDateFin(LocalDate.parse("2020-05-21"));
+		a1.setDescription("blah");
+		a1.setJustification("blah");
+		a1.setEtudiant(etu);
+		
+		Absence	a2 = new Absence();
+		a2.setId(2);
+		a2.setDateDebut(LocalDate.parse("2020-05-25"));
+		a2.setDateFin(LocalDate.parse("2020-05-25"));
+		a2.setDescription("blah");
+		a2.setJustification("blah");
+		a2.setEtudiant(etu);
+		
+		
+		List<Absence> expectedList = new ArrayList<>();
+		expectedList.add(a1);
+		expectedList.add(a2);
+		
+		List<Absence> absenceList = etuService.readAbsenceByEtudiantEmail("ironman@marvel.fr");
+		
+		assertTrue(absenceList.size() == 2);
+		assertEquals(expectedList, absenceList);
+		
+	}
+	
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (1, '2020-05-21', '2020-05-21', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (2, '2020-05-25', '2020-05-25', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from absence", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadAbsenceByInvalidEmail_shouldReturnEmptyList() {
+		
+		List<Note> noteList = etuService.readNoteByEtudiantEmail("batman@marvel.fr");
+		
+		assertThat(noteList).isEmpty();
+		
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (1, '2020-05-21', '2020-05-21', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into absence (id, date_debut, date_fin, description, justification, etudiant_id) values (2, '2020-05-25', '2020-05-25', 'blah', 'blah', 8)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from absence", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadAbsenceByNullEmail_shouldReturnEmptyList() {
+		
+		List<Note> noteList = etuService.readNoteByEtudiantEmail(null);
+		
+		assertThat(noteList).isEmpty();
+		
+	}
+	
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (1, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (2, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (1, 15, 8, 1)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (2, 18, 8, 2)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from Note", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from examen", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadNoteByValidEmail_shouldReturnList() {
+		
+		Etudiant etu = new Etudiant();
+		etu.setId(8);
+		etu.setCni(545684842);
+		etu.setCp(69500);
+		etu.setEmail("ironman@marvel.fr");
+		etu.setNom("Stark");
+		etu.setPrenom("Tony");
+		etu.setNum(235645611);
+		
+		Examen ex1 = new Examen();
+		ex1.setId(1);
+		ex1.setCoef(2);
+		ex1.setDate(LocalDate.parse("2020-05-21"));
+		
+		Examen ex2 = new Examen();
+		ex2.setId(2);
+		ex2.setCoef(2);
+		ex2.setDate(LocalDate.parse("2020-05-21"));
+		
+		Note n1 = new Note();
+		n1.setId(1);
+		n1.setValeur(15);
+		n1.setEtudiant(etu);
+		n1.setExamen(ex1);
+		
+		Note n2 = new Note();
+		n2.setId(2);
+		n2.setValeur(18);
+		n2.setEtudiant(etu);
+		n2.setExamen(ex2);
+		
+		List<Note> expectedList = new ArrayList<>();
+		expectedList.add(n1);
+		expectedList.add(n2);
+		
+		List<Note> noteList = etuService.readNoteByEtudiantEmail("ironman@marvel.fr");
+		
+		assertTrue(noteList.size() == 2);
+		assertEquals(expectedList, noteList);
+		
+	}
+	
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (1, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (2, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (1, 15, 8, 1)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (2, 18, 8, 2)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from Note", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from examen", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadNoteByInvalidEmail_shouldReturnEmptyList() {
+		
+		List<Note> noteList = etuService.readNoteByEtudiantEmail("batman@marvel.fr");
+		
+		assertThat(noteList).isEmpty();
+		
+	}
+	
+	@Test
+	@Sql(statements = "insert into etudiant (id, cni, cp, email, nom, num, prenom) values(8, 545684842, 69500, 'ironman@marvel.fr','Stark', 235645611,'Tony')", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (1, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into examen (id, coef, date) values (2, 2, '2020-05-21') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (1, 15, 8, 1)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "insert into Note (id, valeur, etudiant_id, examen_id) values (2, 18, 8, 2)", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from Note", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from examen", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from etudiant", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	public void testReadNoteByNullEmail_shouldReturnEmptyList() {
+		
+		List<Note> noteList = etuService.readNoteByEtudiantEmail(null);
+		
+		assertThat(noteList).isEmpty();
+		
+	}
 	
 	
 }
