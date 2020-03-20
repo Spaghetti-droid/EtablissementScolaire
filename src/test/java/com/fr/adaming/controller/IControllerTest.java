89e3c12e6633d19d933190a0751edf7c94ddfa4b
jmmathevet <jmmathevet@gmail.com>
2020-03-20 13:16:02 +0100

    Ajout d'une interface pour la couche Controller

---
+++ src/test/java/com/fr/adaming/controller/IControllerTest.java
@@ -0,0 +1,29 @@
+package com.fr.adaming.controller;
+
+public interface IControllerTest {
+
+		
+		public void testCreatingEntityWithValidBody_shouldReturnStatusOk();
+		
+		public void testCreatingEntityWithInvalidBody_shouldReturnBadStatus();
+		
+		public void testDeletingEntityWithValidId_shouldReturnStatusOk();
+		
+		public void testDeletingEntityWithInvalidId_shouldReturnBadStatus();
+		
+		public void testDeletingEntityWithNegativeId_shouldReturnBadStatus();
+		
+		public void testUpdatingEntityWithValidId_shouldReturnStatusOk();
+		
+		public void testUpdatingEntityWithInvalidId_shouldReturnBadStatus();
+		
+		public void testReadingEntityWithValidId_shouldReturnStatusOk();
+		
+		public void testReadingEntityWithInvalidId_shouldReturnStatusOk();
+		
+		public void testReadingEntityWithNegativeId_shouldReturnBadStatus();
+		
+		public void testReadingAllEntity_shouldReturnStatusOk();
+		
+		
+}
