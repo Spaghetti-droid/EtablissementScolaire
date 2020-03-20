7fe6af4da9af0d4c810bc58c2783c38337e76a29
Lea369 <coston.lea@gmail.com>
2020-03-20 17:30:06 +0100

    un truc

--- src/main/java/com/fr/adaming/service/AbstractService.java
+++ src/main/java/com/fr/adaming/service/AbstractService.java
@@ -3,12 +3,9 @@ package com.fr.adaming.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public abstract class AbstractService<E> implements IService<E>  {
 	
 	@Autowired
@@ -31,16 +28,13 @@ public abstract class AbstractService<E> implements IService<E>  {
 
 	@Override
 	public boolean deleteById(Integer id) {
-		try{
+		
 			if(!dao.existsById(id)) {
 			return false;
 		}
 		dao.deleteById(id);
-		return true;}
-		catch(DataIntegrityViolationException e) {
-			log.error("Erreur de suppression" +e);
-			return false;
-		}
+		return true;
+		
 		
 	}
 }
