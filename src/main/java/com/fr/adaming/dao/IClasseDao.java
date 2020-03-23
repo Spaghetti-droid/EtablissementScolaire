ccdc084999bf7e4c15ec9ab1c791fd727e059151
Lea369 <coston.lea@gmail.com>
2020-03-23 12:02:05 +0100

    javadoc DAO

--- src/main/java/com/fr/adaming/dao/IClasseDao.java
+++ src/main/java/com/fr/adaming/dao/IClasseDao.java
@@ -5,8 +5,23 @@ import org.springframework.stereotype.Repository;
 
 import com.fr.adaming.entity.Classe;
 
+
+
+/**
+ * Interface repository pour la class Classe implements JpaRepository<Classe, Integer>
+ * @author Gregoire Brebner
+ * 
+ */
+
 @Repository
 public interface IClasseDao extends JpaRepository<Classe, Integer>{
+	
+	/**
+	 * Methode de recherche par nom
+	 * @param nom
+	 * @return Classe
+	 *
+	 */
 
 	public Classe findByNom (String nom);
 }
--- src/main/java/com/fr/adaming/dao/IMatiereDao.java
+++ src/main/java/com/fr/adaming/dao/IMatiereDao.java
@@ -10,13 +10,36 @@ import org.springframework.stereotype.Repository;
 import com.fr.adaming.entity.Examen;
 import com.fr.adaming.entity.Matiere;
 
+/**
+ * Interface repository pour la class Matiere implements JpaRepository<Matiere, Integer>
+ * @author Gregoire Brebner
+ * 
+ */
 @Repository
 public interface IMatiereDao extends JpaRepository<Matiere, Integer>{
 	
+	/**
+	 * Methode de recherche par nom
+	 * @param nom
+	 * @return Matiere
+	 *
+	 */
 	public Matiere findByNom (String nom);
 	
+	/**
+	 * Methode de existance par nom
+	 * @param nom
+	 * @return boolean
+	 *
+	 */
 	public boolean existsByNom(String nom);
 	
+	/**
+	 * Methode de recherche d'Examen par nom de Matiere
+	 * @param nom
+	 * @return List<Examen>
+	 *
+	 */
 	@Query(value = "From Examen where matiere_id in (select id from Matiere where nom = :nom)")
 	public List<Examen> findExamenByNomMatiere(@Param(value = "nom") String nom);
 
